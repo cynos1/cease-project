a445ab3d3589772cabf3b574316eb81a533bc284
cynos1 <36894951+cynos1@users.noreply.github.com>
2021-02-28 23:43:17 -0500

    update with responsiveness

---
+++ anatomy-face/anatomy-settings.js
@@ -0,0 +1,65 @@
+var anaconfig = {
+  "default":{
+    "outlineColor": "#FF0000"
+  },
+  "ana1":{//full-face
+//info of the popup
+    "url": "https://www.humananatomyillustrations.com/",//link to any webpage
+    "target": "same_window",// use "new_window", "same_window", "modal" with bootstrap only, or "none"
+    "enabled": true//true/false to activate/deactivate
+  },
+  "ana2":{//forehead
+    "hover": "",
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana3":{//eye-rt
+    "hover": "",
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana4":{//eye-lt
+    "hover": "",
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana5":{//nose
+    "hover": "",
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana6":{//mouth
+    "hover": "",
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana7":{//ear-rt
+   /* "hover": "",*/
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana8":{//ear-lt
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana9":{//cheek-rt
+  
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana10":{//cheek-lt
+    
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana11":{//chin
+    
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  },
+  "ana12":{//neck
+    
+    "url": "https://www.humananatomyillustrations.com/", "target": "same_window",
+    "enabled": true
+  }
+};
